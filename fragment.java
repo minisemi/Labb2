7ba28e3a7dcd4086c38d10b7c1b8e23ffdeb7b80
Dotsonen <Dotsonen@gmail.com>
2015-10-06 19:15:27 +0200

    test2

--- fragment.java
+++ fragment.java
@@ -4,6 +4,7 @@ public class Fragment {
 	
 	int qualityLevel;
 	int fragmentSize;
+	int currentlyDownloaded = 0;
 	
 	public Fragment(int qualityLevel){
 		
@@ -12,21 +13,43 @@ public class Fragment {
 		switch(qualityLevel){
 		
 		case 0:
-			fragmentSize = 250000*8;
+			fragmentSize = 250000/8;
 			break;
 		case 1:
-			fragmentSize = 500000*8;
+			fragmentSize = 500000/8;
 			break;
 		case 2:
-			fragmentSize = 850000*8;
+			fragmentSize = 850000/8;
 			break;
 		case 3:
-			fragmentSize = 1300000*8;
+			fragmentSize = 1300000/8;
 			break;
 			
 		
 		}
 		
 	}
+		public int getFragmentSize(){
+			return fragmentSize;
+	}
+		
+		public void setCurrentlyDownloaded(int bandwidth){
+			
+			currentlyDownloaded += bandwidth;
+		}
+		
+		public int getCurrentlyDownloaded(){
+			return currentlyDownloaded;
+		}
+		
+		public boolean downloadCompleted(){
+			
+			if(currentlyDownloaded >= fragmentSize){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
 
 }
